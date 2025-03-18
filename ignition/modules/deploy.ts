d945a24502c98dc487ff4c412e21be3a359a95da
DelassusFelix <delassus.felix@gmail.com>
2025-03-18 11:14:02 +0100

    first commit

---
+++ ignition/modules/deploy.ts
@@ -0,0 +1,9 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const storageModule = buildModule("StorageModule", (m) => {
+  const storage = m.contract("Storage");
+
+  return { storage };
+});
+
+export default storageModule;
